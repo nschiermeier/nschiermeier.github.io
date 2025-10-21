055206400e998ced0fe95eb3eda95bebf04b6b3a
nschiermeier <nschierm@purdue.edu>
2025-10-20 20:12:04 -0700

    Markdown test plus starting the real blog 2

--- blog/posts.js
+++
@@ -1,8 +0,0 @@
-const posts = [
-  {
-    title:"Exploring Blogs",
-    date:"October 15, 2025",
-    summary:"Trying out creating a blog post and messing with formatting and such. Think of this one as the \"Meta Blog\"",
-    content:"<p> This is where the text of a full post would go! This would be a more detailed explanation than the quick snippet seen before. I would add details here about whatever the post is.</p>"
-  }
-];
